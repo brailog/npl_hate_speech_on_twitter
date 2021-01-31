f2f199e452ae84216e03c4e08f0beae5d885358d
brailog <bielrammos@gmail.com>
2021-01-31 17:34:26 -0300

    Integration with twitter real time api and the NPL / Realtime graphing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         '': 'source',
     },
     description='',
-    version='1.0.0',
+    version='1.0.1',
     author='Gabriel Ramos',
     install_requires=[
         "matplotlib == 3.3.4",
