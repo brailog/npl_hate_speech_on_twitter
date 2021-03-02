d9f6819d8521613017b30bf04748f7394d0bca1e
brailog <bielrammos@gmail.com>
2021-03-01 22:39:25 -0300

    Updating to PT-BR analysis and New plot version

--- dataset/loading_tweets_by_id.py
+++ dataset/loading_tweets_by_id.py
@@ -7,10 +7,10 @@ from tweepy.error import TweepError
 from time import sleep
 from bcolor import bcolors
 
-ckey="-"
-csecret="-"
-atoken="-"
-asecret="-"
+ckey="of2lVeoMUiv3iTwDrpA8wssCn"
+csecret="oKvoR4grHquJ2T8jjgtyOlqVAQb6I4cua7PsECsy8tLe0dqwKJ"
+atoken="2555813954-MlvxEwReNJClI9teydlXqapX14fzW2s5usZoU7W"
+asecret="WJS8DZiZJszHfR4sNHASegH2m3ajO9pZxbWbUbr0UrhrF"
 
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
@@ -21,53 +21,71 @@ N_TAG = 'NEGATIVO'
 
 p_array = list()
 n_array = list()
-b = 2250
 
-with open('positivos.txt', 'r') as file:
-    i = 1
-    for line in file:
-        try:
-            sys.stdout.write(bcolors.GREEN)
-            print('{} [{}] - Reading file {} of {} | {}%'.format(time.strftime("%H:%M:%S", time.localtime()), P_TAG, i, b, (i/b)*100))
-            sys.stdout.write(bcolors.RESET)
-            tweet_info = api.get_status(line)
-            text = tweet_info.text
-            p_array.append((text,1))
-            sleep(3)
-            i += 1
-            if i == b:
+p_tweets = list()
+n_tweets = list()
+b = 0
+i = 1
+with open('positivos.txt', 'r') as pos_file:
+    for line in pos_file:
+        p_tweets.append(line)
+        b += 1 
+        if b >= 10000:
+            break 
+b = 0 
+with open('negativos.txt', 'r') as neg_file:
+        for line in neg_file:
+            n_tweets.append(line)
+            b += 1
+            if b >= 10000:
                 break
-        except TweepError:
-            sys.stdout.write(bcolors.RED)
-            print('{} [{}] - Reading file {} of {} | {}%'.format(time.strftime("%H:%M:%S", time.localtime()), P_TAG, i, b, (i/b)*100))
-            sys.stdout.write(bcolors.RESET)
-            i += 1
-            continue
+
+b = len(p_tweets)
+for tweet_id in p_tweets:
+    try:
+        tweet_info = api.get_status(tweet_id)
+        text = tweet_info.text
+        sys.stdout.write(bcolors.GREEN)
+        print('{} [{}] - Adding tweet {} of {} | {}%'.format(time.strftime("%H:%M:%S", time.localtime()), P_TAG, i, b, (i/b)*100))
+        sys.stdout.write(bcolors.RESET)
+        p_array.append((text,1))
+        sleep(3)
+        i += 1
+        if i > b:
+            break
+    except TweepError:
+        sys.stdout.write(bcolors.RED)
+        print('{} [{}] - Failing tweet {} of {} | {}%'.format(time.strftime("%H:%M:%S", time.localtime()), P_TAG, i, b, (i/b)*100))
+        sys.stdout.write(bcolors.RESET)
+        i += 1
+        if i > b:
+            break
+        continue
             
 print('Finish positivos indo para o to csv')
 df = pd.DataFrame(p_array, columns=['tweets', 'label'])
 df.to_csv('p_out.csv', index=False)
-
-with open('negativos.txt', 'r') as file:
-    i = 1
-    for line in file:
-        try:
-            sys.stdout.write(bcolors.GREEN)
-            print('{} [{}] - Reading file {} of {} | {}%'.format(time.strftime("%H:%M:%S", time.localtime()), N_TAG, i, b, (i/b)*100)) 
-            sys.stdout.write(bcolors.RESET)
-            tweet_info = api.get_status(line)
-            text = tweet_info.text
-            n_array.append((text,0))
-            sleep(3)
-            i += 1
-            if i == b:
-                break
-        except TweepError:
-            sys.stdout.write(bcolors.RED)
-            print('{} [{}] - Reading file {} of {} | {}%'.format(time.strftime("%H:%M:%S", time.localtime()), N_TAG, i, b, (i/b)*100))
-            sys.stdout.write(bcolors.RESET)
-            i += 1
-            continue
+i = 1
+for tweets_id in n_tweets:
+    try:
+        tweet_info = api.get_status(tweets_id)
+        text = tweet_info.text
+        sys.stdout.write(bcolors.GREEN)
+        print('{} [{}] - Adding tweet {} of {} | {}%'.format(time.strftime("%H:%M:%S", time.localtime()), N_TAG, i, b, (i/b)*100))
+        sys.stdout.write(bcolors.RESET)
+        n_array.append((text,0))
+        sleep(3)
+        i += 1
+        if i > b:
+            break
+    except TweepError:
+        sys.stdout.write(bcolors.RED)
+        print('{} [{}] - Failing tweet {} of {} | {}%'.format(time.strftime("%H:%M:%S", time.localtime()), N_TAG, i, b, (i/b)*100))
+        sys.stdout.write(bcolors.RESET)
+        i += 1
+        if i > b:
+            break
+        continue
 
 print('Finish negativos indo para o to csv')
 df = pd.DataFrame(n_array, columns=['tweets', 'label'])
--- npl_hate_speech_on_twitter/plot.py
+++ npl_hate_speech_on_twitter/plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
 import time
-
+color = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 style.use("ggplot")
 
 fig = plt.figure()
@@ -11,24 +11,27 @@ ax1 = fig.add_subplot(1,1,1)
 def animate(i):
     pullData = open("twitter-out.txt","r").read()
     lines = pullData.split('\n')
-
-    xar = []
-    yar = []
-
+    
+    values = [0,0,0]
+    name = ['Baixa precisão','Negativo','Positivo']
+    
     x = 0
     y = 0
+    z = 0
 
     for l in lines[-200:]:
-        x += 1
-        if "2" in l:
+        if "1" in l:
+            x += 1
+            values[2] = x
+        elif "0"in l:
             y += 1
-        elif "1" in l:
-            y -= 1
-
-        xar.append(x)
-        yar.append(y)
-        
+            values[1] = y
+        else:
+            z += 1
+            values[0] = z
+    print(sum(values))
+    print(values)
     ax1.clear()
-    ax1.plot(xar,yar)
+    ax1.bar(name, values)
 ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
